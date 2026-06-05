dead71b8cfde15b49aeb8fb402cf57e5d506a86e
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-06-05 17:36:19 +0200

    Rebase from 'debian/kilted/eigenpy'

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -140,13 +140,7 @@ macro(FINDPYTHON)
       if(NOT "${_PYTHON_VERSION_RESULT_VARIABLE}" STREQUAL "0")
         message(FATAL_ERROR "${PYTHON_EXECUTABLE} --version did not succeed.")
       endif()
-      string(
-        REGEX REPLACE
-        "Python "
-        ""
-        _PYTHON_VERSION
-        ${_PYTHON_VERSION_OUTPUT}
-      )
+      string(REPLACE "Python " "" _PYTHON_VERSION ${_PYTHON_VERSION_OUTPUT})
       string(REGEX REPLACE "\\." ";" _PYTHON_VERSION ${_PYTHON_VERSION})
       list(GET _PYTHON_VERSION 0 _PYTHON_VERSION_MAJOR)
 
@@ -328,8 +322,7 @@ macro(FINDPYTHON)
     # Keep compatility with former jrl-cmake-modules versions
     if(PYTHON_DEB_LAYOUT)
       string(
-        REPLACE
-        "site-packages"
+        REPLACE "site-packages"
         "dist-packages"
         PYTHON_SITELIB
         "${PYTHON_SITELIB}"
@@ -340,8 +333,7 @@ macro(FINDPYTHON)
     # directory name
     if(PYTHON_PACKAGES_DIR)
       string(
-        REGEX REPLACE
-        "(site-packages|dist-packages)"
+        REGEX REPLACE "(site-packages|dist-packages)"
         "${PYTHON_PACKAGES_DIR}"
         PYTHON_SITELIB
         "${PYTHON_SITELIB}"
@@ -393,8 +385,7 @@ macro(FINDPYTHON)
     INSTALL_JRL_CMAKEMODULES_FILE("python.cmake")
     INSTALL_JRL_CMAKEMODULES_FILE("python-helpers.cmake")
     string(
-      CONCAT
-      PYTHON_EXPORT_DEPENDENCY_MACROS
+      CONCAT PYTHON_EXPORT_DEPENDENCY_MACROS
       "list(APPEND PYTHON_COMPONENTS ${PYTHON_COMPONENTS})\n"
       "list(REMOVE_DUPLICATES PYTHON_COMPONENTS)\n"
       "if(NOT FINDPYTHON_ALREADY_CALLED)\n"
@@ -420,8 +411,7 @@ macro(FINDPYTHON)
 
   # Log Python variables
   list(
-    APPEND
-    LOGGING_WATCHED_VARIABLES
+    APPEND LOGGING_WATCHED_VARIABLES
     PYTHONINTERP_FOUND
     PYTHONLIBS_FOUND
     PYTHON_LIBRARY_DIRS
@@ -492,8 +482,7 @@ macro(DYNAMIC_GRAPH_PYTHON_MODULE SUBMODULENAME LIBRARYNAME TARGETNAME)
 
   set(PYTHON_INSTALL_DIR "${PYTHON_SITELIB}/dynamic_graph/${SUBMODULENAME}")
   string(
-    REGEX REPLACE
-    "[^/]+"
+    REGEX REPLACE "[^/]+"
     ".."
     PYTHON_INSTALL_DIR_REVERSE
     ${PYTHON_INSTALL_DIR}
